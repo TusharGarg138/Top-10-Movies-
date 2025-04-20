c802622f05fc7c767e325d1d23e5708faaa23e21
Tushar Garg <144510935+TusharGarg138@users.noreply.github.com>
2025-04-20 10:17:23 +0530

    rendering the home page

--- main.py
+++ main.py
@@ -42,3 +42,13 @@ class FindMovieForm(FlaskForm):
     title = StringField("Movie Title", validators=[DataRequired()])
     submit = SubmitField("Add Movie")
 
+@app.route("/")
+def home():
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    all_movies = result.scalars().all() # convert ScalarResult to Python List
+
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+
+    return render_template("index.html", movies=all_movies)

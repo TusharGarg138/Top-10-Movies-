a9926c27e7ca7ec125088be02c4f00e5a1e0f85f
Tushar Garg <144510935+TusharGarg138@users.noreply.github.com>
2025-04-20 10:16:13 +0530

    Update main.py
    creating the sql table

--- main.py
+++ main.py
@@ -23,3 +23,17 @@ class Base(DeclarativeBase):
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
+#CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()

0ff25405a2b2c925339d826f39712fe7f682875f
Tushar Garg <144510935+TusharGarg138@users.noreply.github.com>
2025-04-20 10:13:45 +0530

    Create main.py
    import all the libraries

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests

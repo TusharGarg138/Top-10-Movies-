9c030320e1f2c14ffecb04384449889be0e4e6b0
Tushar Garg <144510935+TusharGarg138@users.noreply.github.com>
2025-04-20 10:15:21 +0530

    Update main.py
    initializing the keys

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+
+MOVIE_DB_API_KEY = "Your_API_key"
+MOVIE_DB_SEARCH_URL = "themoviedb"
+MOVIE_DB_INFO_URL = "themoviedb"
+MOVIE_DB_IMAGE_URL = "URL"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'seceret key'
+Bootstrap5(app)
+
+##CREATE DB
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
